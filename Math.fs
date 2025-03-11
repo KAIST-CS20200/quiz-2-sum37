6956ef5a9cdab26a356b69ab369e4b852a17acb2
sum37 <82599165+sum37@users.noreply.github.com>
2025-03-11 10:32:10 +0900

    edit slightly

--- Math.fs
+++ Math.fs
@@ -5,16 +5,16 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let isEven n =
+let isEven (n:uint64) =
   if n%2UL =0UL then true
   else false
 
 let collatz n =
   let rec iter (n:uint64) counter =
     if n=1UL then counter
-    elif isEven (uint64 n) then iter ((uint64 n) /2UL) (counter+1UL)
-    else iter (3UL*(uint64 n)+1UL) (counter+1UL)
-  iter (uint64 n) 0UL
+    elif isEven n then iter (n/2UL) (counter+1)
+    else iter (3UL*n+1UL) (counter+1)
+  iter (uint64 n) 0
 
 //오ㅐ 틀렸냐면 overflow때문임. 그래서 문제가 발생한 거임. 끄어억 unsigned로 해보자
 
